437c4e63c30707114a0c14ff6e09cfd32507502e
TanuGangrade <81514718+TanuGangrade@users.noreply.github.com>
2021-04-02 11:49:40 +0400

    Create bs-10.cpp

---
+++ Week_1/BS/bs-10.cpp
@@ -0,0 +1,53 @@
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int median(int a[], int n)
+{
+	if (n%2 == 0)
+		return (a[n/2] + a[n/2-1])/2;
+	else
+		return a[n/2];
+}
+int getMedian(int a1[], int a2[], int n)
+{
+	if (n <= 0)
+		return -1;
+	if (n == 1)
+		return (a1[0] + a2[0])/2;
+	if (n == 2)
+		return (max(a1[0], a2[0]) + min(a1[1], a2[1])) / 2;
+
+	int m1 = median(a1, n);
+	int m2 = median(a2, n); 
+
+	if (m1 == m2)
+		return m1;
+
+	if (m1 < m2)
+	{
+		if (n % 2 == 0)
+			return getMedian(a1 + n/2 - 1, a2, n - n/2 +1);
+		return getMedian(a1 + n/2, a2, n - n/2);
+	}
+
+	
+	if (n % 2 == 0)
+		return getMedian(a2 + n/2 - 1, a1, n - n/2 + 1);
+	return getMedian(a2 + n/2, a1, n - n/2);
+}
+
+
+
+int main()
+{
+	int a1[] = {1, 2, 3, 6};
+	int a2[] = {4, 6, 8, 10};
+	int n1 = sizeof(a1)/sizeof(a1[0]);
+	int n2 = sizeof(a2)/sizeof(a2[0]);
+	if (n1 == n2)
+		printf("Median is %d", getMedian(a1, a2, n1));
+	else
+		printf("Doesn't work for arrays of unequal size");
+	return 0;
+}
