a00798cae38e23ce538d270153c290695680d4d9
TanuGangrade <81514718+TanuGangrade@users.noreply.github.com>
2021-04-02 17:54:39 +0400

    Update 1-6.cpp

--- Week_1/bit/1-6.cpp
+++ Week_1/bit/1-6.cpp
@@ -60,7 +60,16 @@
     }
    
    
-5) 
+5)
+  
+vector <int> rotate (int n, unsigned int d)
+        {
+            vector<int> ans(2);
+            int x=16-d;
+            ans[0]= n<<d|n>>x;
+            ans[1]= n>>d|(n<<x);
+            return ans;
+        }
 
 
 6)int main()
