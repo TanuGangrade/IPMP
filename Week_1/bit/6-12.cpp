b3eba34ee2a16cb484fff2190e4ab092c6f56a62
TanuGangrade <81514718+TanuGangrade@users.noreply.github.com>
2021-04-02 19:08:38 +0400

    Update 6-12.cpp

--- Week_1/bit/6-12.cpp
+++ Week_1/bit/6-12.cpp
@@ -10,4 +10,57 @@
     }
 
 8)
+        
+      int add(int a,int b)
+      {
+            while(b)
+            {
+                  int c=a&b;
+                  a=a^b;
+                  b=c<<1;
+
+            }
+            return a;
+
+      }
   
+9) unsigned int getFirstSetBitPos(int n)
+{
+    return log2(n & -n)+1;
+}
+
+10) unsigned int swapBits(unsigned int x) 
+{ 
+       unsigned int even=x& 0xAAAAAAAA; 
+ 
+    unsigned int odd=x& 0x55555555; 
+  
+    even >>= 1; 
+    odd <<= 1;
+  
+    return (even| odd);
+} 
+
+11) 
+      
+      int po2(int n)
+{
+    return n&&(!(n&(n-1)));
+}
+  
+int findPosition(int n)
+{
+    if (!po2(n))
+        return -1;
+  
+    int i=1, p=1;
+  
+    while (!(i&n)) {
+        i =i<<1;
+          ++p;
+    }
+  
+    return p;
+}
+
+12)
