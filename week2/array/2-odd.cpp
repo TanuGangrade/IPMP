a88d6775a9c4e43a98a16a03d271129f87119490
TanuGangrade <81514718+TanuGangrade@users.noreply.github.com>
2021-04-06 23:58:43 +0400

    Update 2-odd.cpp

--- week2/array/2-odd.cpp
+++ week2/array/2-odd.cpp
@@ -1,29 +1,11 @@
-int getOddOccurrence(int b[], int n) {
+ int getOddOccurrence(int arr[], int n) {
+        // code herei
+        int s=0;
         
-                map<int,int>map;
-
-        
-         for(int i=0;i<n;i++)
+        for(int i=0;i<n;i++)
     {
-        if(map.find(b[i])==map.end())
-        		map.insert(pair<int, int>(b[i],1));
-        		
-        else
-        map[b[i]]++;
-
+        s^=arr[i];
     }
         
-        int ans=0;
-         for(auto i=map.begin();i!=map.end();i++)
-	{	
-	    if(i->second%2!=0)
-        {ans=i->first;
-            break;
-        }
-        
-	}
-	return ans;
-        
-        
-        
+        return s;
     }
