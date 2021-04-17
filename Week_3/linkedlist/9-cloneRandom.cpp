c7de82ec2cdc9b9c328c3cd5b840866ffb97cc60
TanuGangrade <81514718+TanuGangrade@users.noreply.github.com>
2021-04-17 19:09:03 +0400

    Update 9-cloneRandom.cpp

--- Week_3/linkedlist/9-cloneRandom.cpp
+++ Week_3/linkedlist/9-cloneRandom.cpp
@@ -39,3 +39,59 @@ Node* clone(Node *start)
   
     return temp;
 }
+
+
+
+
+
+//-------//
+
+Node* clone(Node *start)
+{
+	Node* curr = start, *temp;
+
+	// insert additional node after
+	// every node of original list
+	while (curr)
+	{
+		temp = curr->next;
+
+		// Inserting node
+		curr->next = new Node(curr->data);
+		curr->next->next = temp;
+		curr = temp;
+	}
+
+	curr = start;
+
+	// adjust the random pointers of the
+	// newly added nodes
+	while (curr)
+	{
+		
+			curr->next->random = curr->random ?
+								curr->random->next : NULL;
+
+		// move to the next newly added node by
+		// skipping an original node
+		curr = curr->next->next;
+	}
+
+	Node* original = start, *copy = start->next;
+
+	// save the start of copied linked list
+	temp = copy;
+
+	// now separate the original list and copied list
+	while (original && copy)
+	{
+		original->next =
+		original->next? original->next->next : original->next;
+
+		copy->next = copy->next?copy->next->next:copy->next;
+		original = original->next;
+		copy = copy->next;
+	}
+
+	return temp;
+}
