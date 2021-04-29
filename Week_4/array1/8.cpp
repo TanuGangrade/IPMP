00a7813adfdb3b73c11ed5377ec1e25684acb3d7
TanuGangrade <81514718+TanuGangrade@users.noreply.github.com>
2021-04-29 09:40:50 +0400

    Create 8.cpp

---
+++ Week_4/array1/8.cpp
@@ -0,0 +1,20 @@
+    vector<long long int> productExceptSelf(vector<long long int>& nums, int n) {
+       vector<long long int> prod(n,1); long long int temp=1;
+        //code here      
+        
+        for(int i=0;i<n;i++)
+        {
+            prod[i]=temp;
+            temp*=nums[i];
+            
+        }
+        
+        temp=1;
+       for(int i=n-1;i>=0;i--)
+       {
+           prod[i]*=temp;
+           temp*=nums[i];
+       }
+        
+        return prod;
+    }
