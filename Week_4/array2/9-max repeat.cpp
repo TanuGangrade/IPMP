c8b033fd620aa47e37bd83253180e38fd7bb5c14
TanuGangrade <81514718+TanuGangrade@users.noreply.github.com>
2021-05-02 02:52:02 +0400

    Update 9-max repeat.cpp

--- Week_4/array2/9-max repeat.cpp
+++ Week_4/array2/9-max repeat.cpp
@@ -10,16 +10,16 @@ int most_frequent_element(std::vector<int>& v, int k) {
           int mf = INT_MIN;
           int max = INT_MIN;
           for(int i = 0; i < v.size(); ++i) {
-              int freq = (v[i] - v[i]%sz)/sz;
-            
-            if(mf < freq) {
-              mf = freq;
-              max = i;
-              } 
-            
-            else if(mf == freq && i < max) {
-              max = i;
-              }
+                  int freq = (v[i] - v[i]%sz)/sz;
+
+                if(mf < freq) {
+                  mf = freq;
+                  max = i;
+                  } 
+
+                else if(mf == freq && i < max) {
+                  max = i;
+                  }
           }
           return max;
 }
