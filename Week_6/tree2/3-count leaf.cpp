f155d172449bbaa98c9b86b1f96b20f3aab4b544
TanuGangrade <81514718+TanuGangrade@users.noreply.github.com>
2021-05-27 23:12:20 +0400

    Update 3-count leaf.cpp

--- Week_6/tree2/3-count leaf.cpp
+++ Week_6/tree2/3-count leaf.cpp
@@ -16,3 +16,15 @@ int countLeaves(Node* root)
    return count;
   
 }
+
+///////////////////
+int countLeaves(Node* root)
+{
+   if(root==NULL)
+       return 0;
+       
+       if(root->left==0&&root->right==0)
+       return 1;
+       return countLeaves(root->left) +countLeaves(root->right);
+  
+}
