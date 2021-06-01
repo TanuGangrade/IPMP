462454a51a147c8d78c816e260b59cf25bd824d0
TanuGangrade <81514718+TanuGangrade@users.noreply.github.com>
2021-06-01 23:07:33 +0400

    Create 5-tree isomorphic.cpp

---
+++ Week_7/trees1/5-tree isomorphic.cpp
@@ -0,0 +1,17 @@
+ bool isIsomorphic(Node *root1,Node *root2)
+    {
+    //add code here.
+    if(root1==NULL&&root2==NULL)
+    return 1;
+    if(root1==NULL&&root2)
+    return 0;
+    if(root1&&root2==NULL)
+    return 0;
+    
+    if(root1->data!=root2->data)
+    return 0;
+    
+    return isIsomorphic(root1->left,root2->left)&&isIsomorphic(root1->right,root2->right)||
+            isIsomorphic(root1->left,root2->right)&&isIsomorphic(root1->right,root2->left);
+    
+    }
