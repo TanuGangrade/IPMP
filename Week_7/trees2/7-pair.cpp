4cce784e13bd09ae63bddb618b310dc85525fe17
TanuGangrade <81514718+TanuGangrade@users.noreply.github.com>
2021-06-04 02:21:28 +0400

    Create 7-pair.cpp

---
+++ Week_7/trees2/7-pair.cpp
@@ -0,0 +1,33 @@
+ void util(Node*root,vector<int>&v)
+    {
+        if(root==NULL)
+        return;
+        util(root->left,v);
+        
+        v.push_back(root->data);
+        
+        util(root->right,v);
+        
+        
+    }
+    
+    
+    int isPairPresent(struct Node *root, int target)
+    {
+    //add code here.
+    vector<int>v;
+    util(root,v);
+    
+    int i=0,j=v.size()-1;
+    
+    while(i<j)
+    { 
+        if(target==v[i]+v[j])
+         return 1;
+         else if(target>v[i]+v[j])
+         i++;
+         else j--;
+     }
+    
+    return 0;
+    }
