04a76ba95f25daa1eecc1a6bc85fec533866d31f
Tanu Gangrade <81514718+TanuGangrade@users.noreply.github.com>
2021-06-17 00:46:23 +0400

    Create 2-depth fs.cpp

---
+++ Week_9/graph/2-depth fs.cpp
@@ -0,0 +1,23 @@
+class Graph
+{
+public:
+map<int,bool>visited;
+map<int, list<int>> adj;
+
+void util(int v ,vector<int>&ans)
+{
+    visited[v] = true;
+    ans.push_back(v);
+ 
+    list<int>::iterator i;
+    for (i = adj[v].begin(); i != adj[v].end(); ++i)
+        if (!visited[*i])
+            DFS(*i);
+}
+
+  vector<int>DFS(map<int, list<int>> adj)
+  {
+    vector<int>ans;
+    util(0,ans);
+    return ans;
+  }
