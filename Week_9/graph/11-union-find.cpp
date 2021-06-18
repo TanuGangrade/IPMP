83c7f242782547370790aaa8c05cdcb9382df76d
Tanu Gangrade <81514718+TanuGangrade@users.noreply.github.com>
2021-06-19 02:39:25 +0400

    Create 11-union-find.cpp

---
+++ Week_9/graph/11-union-find.cpp
@@ -0,0 +1,28 @@
+int find(int parent[], int i)
+{
+    if (parent[i] == -1)
+        return i;
+    return find(parent, parent[i]);
+}
+ 
+void Union(int parent[], int x, int y)
+{
+    parent[x] = y;
+}
+ 
+int isCycle(Graph* graph)
+{
+    int* parent = new int[graph->V * sizeof(int)];
+    memset(parent, -1, sizeof(int) * graph->V);
+ 
+    for (int i = 0; i < graph->E; ++i) {
+        int x = find(parent, graph->edge[i].src);
+        int y = find(parent, graph->edge[i].dest);
+ 
+        if (x == y)
+            return 1;
+ 
+        Union(parent, x, y);
+    }
+    return 0;
+}
