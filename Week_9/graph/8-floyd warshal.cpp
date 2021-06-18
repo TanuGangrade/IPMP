a5b2d3f0cdbb5bff6fa07ef788e2070386fbb6d7
Tanu Gangrade <81514718+TanuGangrade@users.noreply.github.com>
2021-06-18 15:15:57 +0400

    Update 8-floyd warshal.cpp

--- Week_9/graph/8-floyd warshal.cpp
+++ Week_9/graph/8-floyd warshal.cpp
@@ -11,9 +11,7 @@ void floydWarshall(int graph[][V])
     for (k = 0; k < V; k++) {
         for (i = 0; i < V; i++) {
             for (j = 0; j < V; j++) {
-                if (dist[i][j] > (dist[i][k] + dist[k][j])
-                    && (dist[k][j] != INF
-                        && dist[i][k] != INF))
+                if (dist[i][j] > (dist[i][k] + dist[k][j]) && (dist[k][j] != INF && dist[i][k] != INF))
                     dist[i][j] = dist[i][k] + dist[k][j];
             }
         }
