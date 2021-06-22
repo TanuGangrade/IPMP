b632462f7683e786de15cf9d1501aeca3c9daa62
Tanu Gangrade <81514718+TanuGangrade@users.noreply.github.com>
2021-06-22 14:42:11 +0400

    Create 1-bipertite.cpp

---
+++ Week_10/graph/1-bipertite.cpp
@@ -0,0 +1,33 @@
+bool check(int src,vector<vector<int>>&graph,vector<int>color){
+        queue<int>q;
+        q.push(src);
+        color[src]=1;
+        while(!q.empty()){
+            int node=q.front();
+            q.pop();
+            
+            for(auto it:graph[node]){
+                if(color[it]==-1){
+                    color[it]=1-color[node];
+                    q.push(it);
+                }
+                else if(color[it]==color[node]){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+    
+    bool isBipartite(vector<vector<int>>& graph) {
+        vector<int>color(105,-1);
+        
+        for(int i=0;i<graph.size();i++){
+            if(color[i]==-1){
+                if(!check(i,graph,color)){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
