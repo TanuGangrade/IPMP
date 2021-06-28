86af16dd7d776a42e269b6115ded6117cea52109
Tanu Gangrade <81514718+TanuGangrade@users.noreply.github.com>
2021-06-28 16:35:17 +0400

    Update 14-independent set problem.cpp

--- Week_10/dp/14-independent set problem.cpp
+++ Week_10/dp/14-independent set problem.cpp
@@ -17,16 +17,15 @@ int LISS(node *root)
     if (root->left == NULL && root->right == NULL)
         return (root->liss = 1);
  
-    int liss_excl = LISS(root->left) + LISS(root->right);
+    int exclude = LISS(root->left) + LISS(root->right);
  
-    int liss_incl = 1;
+    int include = 1;
     if (root->left)
-        liss_incl += LISS(root->left->left) + LISS(root->left->right);
+        include += LISS(root->left->left) + LISS(root->left->right);
     if (root->right)
-        liss_incl += LISS(root->right->left) + LISS(root->right->right);
+        include += LISS(root->right->left) + LISS(root->right->right);
  
-    root->liss = max(liss_incl, liss_excl);
+    root->liss = max(include, exclude);
  
     return root->liss;
 }
- 
