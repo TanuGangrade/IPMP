03612dd3bd55f9eb1815631ca7996c152f6c3e0e
Tanu Gangrade <81514718+TanuGangrade@users.noreply.github.com>
2021-08-01 23:55:42 +0400

    Update 2-depth fs.cpp

--- Week_9/graph/2-depth fs.cpp
+++ Week_9/graph/2-depth fs.cpp
@@ -12,7 +12,7 @@ void util(int v ,vector<int>&ans)
     list<int>::iterator i;
     for (i = adj[v].begin(); i != adj[v].end(); ++i)
         if (!visited[*i])
-            DFS(*i);
+            util(*i);
 }
 
   vector<int>DFS(map<int, list<int>> adj)
